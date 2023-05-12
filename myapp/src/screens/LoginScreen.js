8abe6b5e38b6b6761b9bad02eaf59ae1de80087c
mianmiantea2019 <mianmiantea2019@gmail.com>
2023-05-12 18:58:12 -0400

    update login

--- myapp/src/screens/LoginScreen.js
+++ myapp/src/screens/LoginScreen.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./LoginScreen.css";
-import SignupScreen from "./SignupScreen";
 
 function LoginScreen() {
   const [signIn, setSignIn] = useState(false);
@@ -13,6 +12,24 @@ function LoginScreen() {
         </button>
 
         <div className="loginScreen__gradient" />
+      </div>
+      <h1>Unlimited films, TV programmes and more.</h1>
+      <h2>Watch anywhere. Cancel at any time.</h2>
+      <h3>
+        Ready to watch? Enter your email to create or restart your
+        membership.
+      </h3>
+
+      <div className="loginScreen__input">
+        <form>
+          <input type="email" placeholder="Email Address" />
+          <button
+            onClick={() => setSignIn(true)}
+            className="loginScreen__getStarted"
+          >
+            GET STARTED
+          </button>
+        </form>
       </div>
     </div>
 
