360736e53e198eadd44f51eb9b0462d52d500225
mianmiantea2019 <mianmiantea2019@gmail.com>
2023-05-12 16:42:12 -0400

    adjust modal style

--- myapp/src/Modal.js
+++ myapp/src/Modal.js
@@ -2,11 +2,19 @@ import React from "react";
 import "./Modal.css";
 
 function Modal({ movie, onClose }) {
+    const base_url = "https://image.tmdb.org/t/p/original/";
+    const imageUrl = `${base_url}${movie.poster_path}`;
+
     return (
         <div className="modal">
             <div className="modal__content">
                 <h3>{movie.title}</h3>
-                <p>{movie.overview}</p>
+                <img
+                    className="modal__image"
+                    src={imageUrl}
+                    alt={movie.title}
+                />
+                <p className="modal__overview">{movie.overview}</p>
                 <button className="modal__close" onClick={onClose}>
                     Close
                 </button>
