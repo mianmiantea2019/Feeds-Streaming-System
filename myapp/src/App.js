407e347beb043e1eebc2708f62fb6cc1d4ae82a4
mianmiantea2019 <mianmiantea2019@gmail.com>
2023-05-12 18:35:34 -0400

    update router setup

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
-import { BrowserRouter, Router, Route, Redirect, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LoginScreen from './screens/LoginScreen';
 
 const App = () => {
-
+  const user = "sad";
   return (
     <div className="app">
-      <BrowserRouter> 
-      <Routes>
-        <Route exact path="/" element={<HomeScreen />} />
-        <Route path="/about" element={<HomeScreen />} />
-      </Routes> 
-      </BrowserRouter> 
+      <BrowserRouter>
+        <Routes>
+          {!user ? (
+            <Route path="/*" element={<LoginScreen />} />
+          ) : (
+            <>
+              <Route path="/" element={<HomeScreen />} />
+              {/* <Route path="/about" element={<HomeScreen />} /> */}
+              {/* <Route path="/*" element={<Navigate to="/" />} /> */}
+            </>
+          )}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
-
 
 export default App;
