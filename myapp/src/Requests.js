d27b7908200981395ec228b12b7f5a17012230a0
mianmiantea2019 <mianmiantea2019@gmail.com>
2023-05-12 15:01:49 -0400

    setup environment variable

--- myapp/src/Requests.js
+++ myapp/src/Requests.js
@@ -1,5 +1,4 @@
-// Typically we would store in {process.env.API_KEY}
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
