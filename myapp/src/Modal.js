14fa5d9c18bdf684b8c1f0ee23093e07dd173d1b
mianmiantea2019 <mianmiantea2019@gmail.com>
2023-05-12 16:45:13 -0400

    for better user expereince on modal

--- myapp/src/Modal.js
+++ myapp/src/Modal.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react"; 
 import "./Modal.css";
 
 function Modal({ movie, onClose }) {
     const base_url = "https://image.tmdb.org/t/p/original/";
     const imageUrl = `${base_url}${movie.poster_path}`;
+    const modalRef = useRef(null);
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if (modalRef.current && !modalRef.current.contains(event.target)) {
+                onClose();
+            }
+        };
+
+        document.addEventListener("mousedown", handleOutsideClick);
+
+        return () => {
+            document.removeEventListener("mousedown", handleOutsideClick);
+        };
+    }, [onClose]);
+
 
     return (
         <div className="modal">
-            <div className="modal__content">
+            <div className="modal__content" ref={modalRef}>
                 <h3>{movie.title}</h3>
                 <img
                     className="modal__image"
