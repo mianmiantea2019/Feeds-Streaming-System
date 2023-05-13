bf7bb2e4a5d99bb5c6b17a62631b45a1be4f88e4
mianmiantea2019 <mianmiantea2019@gmail.com>
2023-05-13 08:52:42 -0400

    create profile screen

--- myapp/src/screens/ProfileScreen.js
+++ myapp/src/screens/ProfileScreen.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
+import { auth } from "../firebase";
+import Nav from "../Nav";
+import PlansScreen from "./PlansScreen";
 import "./ProfileScreen.css";
 
 function ProfileScreen() {
@@ -8,7 +11,30 @@ function ProfileScreen() {
 
   return (
     <div className="profileScreen">
-     this is progile
+      <Nav />
+      <div className="profileScreen__body">
+        <h1>Edit Profile</h1>
+        <div className="profileScreen__info">
+          <img
+            src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+            alt=""
+          />
+          <div className="profileScreen__details">
+            <h2>{user.email}</h2>
+            <div className="profileScreen__plans">
+              <h3>Plans</h3>
+
+              <PlansScreen />
+              <button
+                onClick={() => auth.signOut()}
+                className="profileScreen__signOut"
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
