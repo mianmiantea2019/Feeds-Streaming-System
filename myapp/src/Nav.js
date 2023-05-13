dacff84cac080cde2b14c6b772c4f6d819b28fbb
mianmiantea2019 <mianmiantea2019@gmail.com>
2023-05-12 23:35:47 -0400

    updte navbar style

--- myapp/src/Nav.js
+++ myapp/src/Nav.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./Nav.css";
 // import logo from "./image/logo.JPG";
+import { useNavigate } from "react-router-dom";
 
 function Nav() {
   const [show, handleShow] = useState(false);
+  const navigate = useNavigate();
+
 
   const transitionNavBar = () => {
     if (window.scrollY > 100) {
@@ -27,21 +30,35 @@ function Nav() {
           alt="MOVIELAND"
         /> */}
         <div className="nav__logo">
-        <h1 style={{
-          display:"inline",
-          fontSize: "24px",
-          fontWeight: "bold",
-          color: "Grey",
-          textTransform: "uppercase",
-        }}>
-          MovieLand
-        </h1>
+          <h1 style={{
+            fontSize: "24px",
+            fontWeight: "bold",
+            color: "Grey",
+            textTransform: "uppercase",
+          }}
+            onClick={() => navigate("/")}
+          >
+            MovieLand
+          </h1>
         </div>
-        <img
+        <div className="nav__avatar">   
+          <h1 style={{
+          display: "inline",
+          color: "Grey",
+          fontSize: "20px",
+        }}
+          onClick={() => navigate("/profile")}
+        >
+          MyProfile
+        </h1>    
+         </div>
+        {/* <img
           className="nav__avatar"
-          src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
-          alt=""
-        />
+          src="https://img.tukuppt.com/ad_preview/00/59/71/sXRbYpHNLL.jpg!/both/260x260"
+          alt="image1"
+          onClick={() => navigate("/profile")}
+        /> */}
+
       </div>
     </div>
   );
